daa80ba372431fe888d3a6fb778a1f34aac65df2
John Lyell <jjlyell@gmail.com>
2018-10-07 02:53:20 +0000

    api

---
+++ APIs/firstReq/req.js
@@ -0,0 +1,10 @@
+var request = require('request');
+request('http://www.google.com', function(error, response, body){
+    if(error){
+        console.log("There was an error.");
+        console.log(error);
+    }else if(response.statusCode === 200){
+        console.log(body);
+    }
+});
+
