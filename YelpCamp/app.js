6801ef02814e7fae64aacf7b095dee9e69602505
John Lyell <jjlyell@gmail.com>
2018-10-11 03:24:21 +0000

    yelpcamp db

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,37 +1,60 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name:String,
+    image:String,
+});
 
-var campgrounds = [
-   {name: "Bass River", image: "https://c2.staticflickr.com/8/7215/7182369178_5572295ce0.jpg"},
-   {name: "Badger Prairie", image: "https://c1.staticflickr.com/5/4044/4175370953_5488caf554.jpg"},
-   {name: "Willow Rest", image: "https://c1.staticflickr.com/5/4082/4756767667_fe48b8e42a.jpg"},
-   ]; 
+var Campground = mongoose.model("Campground", campgroundSchema);
 
+// Campground.create({name: "Willow Rest", image: "https://c1.staticflickr.com/5/4082/4756767667_fe48b8e42a.jpg"}, function(err, campground){
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log(campground);
+//     }
+// });
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+    
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name:name, image:image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds") //defaults to get request!
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+             res.redirect("/campgrounds"); //defaults to get request if repeat!
+        }
+    });
+   
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new")
-})
+    res.render("new");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp server has started...");
