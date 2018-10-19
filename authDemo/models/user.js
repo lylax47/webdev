14a35ae3131ada1749e9c4a7058e28e26efa87de
John Lyell <jjlyell@gmail.com>
2018-10-19 00:55:54 +0000

    authentication'

---
+++ authDemo/models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose); //Will add import function for user auth
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
